0cb90920a58e50bb8e0c5243f4555941b363f2f4
Huy Phúc <thienxa2820022@gmail.com>
2022-04-10 22:16:31 +0700

    set up server

---
+++ server/controllers/jobsController.js
@@ -0,0 +1,22 @@
+const createJob = (req, res) => {
+    res.send('createJob')
+}
+
+const getAllJobs = (req, res) => {
+    res.send('getAllJobs')
+}
+
+
+const updateJobs = (req, res) => {
+    res.send('updateJobs')
+}
+
+const deleteJobs = (req, res) => {
+    res.send('updateJobs')
+}
+const showJobs = (req, res) => {
+    res.send('updateJobs')
+}
+
+
+module.exports = { register,login,updateUser}
