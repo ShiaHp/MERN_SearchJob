83b027dd3e93162803875060566d7b5ee92de83d
Huy Phúc <thienxa2820022@gmail.com>
2022-04-11 21:42:36 +0700

    proxy , connect

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv')
 dotenv.config()
+const cors = require('cors');
 
+app.use(cors());
 require('express-async-errors');
 // routes
 const authRouter = require('./routes/authRoutes')
@@ -16,8 +18,11 @@ const  notFoundMiddleware  = require('./middleware/not-found');
 
 
 app.use(express.json())
-app.get('/', (req, res) => {
-    res.send('Welcome to middleware')
+// app.get('/', (req, res) => {
+//     res.json({msg : 'Welcome!'})
+// })
+app.get('/api/v1', (req, res) => {
+    res.json({msg : 'API'})
 })
 
 app.use('/api/v1/auth',authRouter)
