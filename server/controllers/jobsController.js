e733ef49943c81c5509d6d9ae540487c79a81555
Huy Phúc <thienxa2820022@gmail.com>
2022-04-18 18:15:22 +0700

    monlyapplication using arge mongoose

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -3,6 +3,7 @@ const {StatusCodes} = require('http-status-codes');
 const {BadRequestError,NotFoundError,Unauthenticated} = require('../errors/index.js');
 const  checkPermissions  = require('../utils/checkPermissions');
 const mongoose = require('mongoose')
+const moment = require('moment');
 const createJob = async (req, res) => {
       const { position,company} = req.body;
 
@@ -92,7 +93,20 @@ const showStats = async (req, res) => {
         declined : stats.declined || 0
     }
 
-    let monthlyApplication =[]
+    let monthlyApplication = await Job.aggregate([
+        {$match : {createdBy : mongoose.Types.ObjectId(req.user.userId)}},
+        {$group : {_id : {year :{$year : '$createdAt'} , month :{$month : '$createdAt'}},
+        count : {$sum : 1}
+        } },
+        {$sort : {'_id.year' : -1,'_id.month' : -1}},
+        {$limit : 6}
+
+    ])
+    monthlyApplication=    monthlyApplication.map((item,index) =>{
+        const {_id :{year,month} , count} = item;
+        const date= moment().month(month-1).year(year).format('MMM Y')
+        return{date,count}
+    }).reverse()
     res.status(StatusCodes.OK).json({message: 'Success',defaultStats  , monthlyApplication})
 }
 
